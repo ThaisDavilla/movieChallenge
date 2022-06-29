259a8ee6c7df4233b12b1e05804d77e9454ac676
thaisdavilla <thaisdavilla@yahoo.com.br>
2022-06-29 10:43:46 -0300

    Resolução do desafio com Array e objetos.

---
+++ script.js
@@ -0,0 +1,55 @@
+//Crie 3 variáveis, cada uma com um array: Comedy - Action - Fantasy
+//Dentro de cada array, insira 3 (TRÊS) Objetos, contendo: title, director, year
+
+let comedy = [{
+  Title: 'Tag',
+  Director: 'Jeff Tomsic',
+  Year: 2018,
+},{
+  Title:'Grouwn Ups',
+  Director:'Dennis Dugan',
+  Year: 2010 ,
+},{ 
+  Title:"'Don't Look Up'",
+  Director:' Adam McKay',
+  Year: 2021,
+}]
+
+let action = [{
+  Title:'The Batman',
+  Director:'Matt Reeve',
+  Year: 2022,
+},{
+  Title:'Tenet',
+  Director:'Christopher Nolan',
+  Year: 2020,
+},{
+  Title: 'The Departed',
+  Director:' Martin Scorsese',
+  Year: 2006,
+}]
+
+let fantasy = [{
+  Title:'Harry Potter and the Order of the Phoenix.',
+  Director:'  David Yates',
+  Year: 2007,
+},{
+  Title:'Ready Player One.',
+  Director:' Steven Spielbergs',
+  Year: 2018,
+},{
+  Title:'Spider-man: no way home',
+  Director:'Jon watts',
+  Year: 2021,
+}]
+
+//Escolha uma variável e insira uma condição para ela, (condição de sua escolha), com a seguinte mensagem ao console: 
+//IF: "MY FAVORIT MOVIE IS..."
+//ELSE: "NOT EVEN 5 STARS..." 
+
+if(comedy[0] === comedy[0]){
+  console.log('My favorit movie is "TAG"')
+}else{
+  console.log('Not even 5 stars')
+}
+console.log(comedy[0])
