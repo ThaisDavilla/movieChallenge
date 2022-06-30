5e7394be19f52c28c73aab9ce8ec2fdad9d28967
thaisdavilla <thaisdavilla@yahoo.com.br>
2022-06-30 17:52:50 -0300

    Finalização do desafio.

--- script.js
+++ script.js
@@ -14,6 +14,13 @@ let comedy = [{
   Director:' Adam McKay',
   Year: 2021,
 }]
+console.table(comedy)
+//Desafio extra
+if(comedy = 'Tag'){
+  console.log("MY FAVORIT MOVIE IS TAG!")
+}else{
+  console.log("NOT EVEN 5 STARS!")
+}
 
 let action = [{
   Title:'The Batman',
@@ -28,6 +35,13 @@ let action = [{
   Director:' Martin Scorsese',
   Year: 2006,
 }]
+console.table(action)
+//Desafio extra
+if(action = 'Tenet'){
+  console.log("MY FAVORIT MOVIE IS TENET!")
+}else{
+  console.log("NOT EVEN 5 STARS!")
+}
 
 let fantasy = [{
   Title:'Harry Potter and the Order of the Phoenix.',
@@ -42,11 +56,19 @@ let fantasy = [{
   Director:'Jon watts',
   Year: 2021,
 }]
-
+console.table(fantasy)
+//DESAFIO EXTRA
+if(fantasy = 'Harry Potter and the Order of the Phoenix.'){
+  console.log("MY FAVORIT MOVIE IS Harry Potter and the Order of the Phoenix!")
+}else{
+  console.log("NOT EVEN 5 STARS!")
+}
+//dESAFIO EXTRA:
 //Escolha uma variável e insira uma condição para ela, (condição de sua escolha), com a seguinte mensagem ao console: 
 //IF: "MY FAVORIT MOVIE IS..."
 //ELSE: "NOT EVEN 5 STARS..." 
 
-//Vou fazer!!!
+
+
 
 
